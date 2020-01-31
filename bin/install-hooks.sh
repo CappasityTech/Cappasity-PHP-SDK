6bcfc2c40147bfb1e8fb1b6cf1b537986aeba632
BelkinaDasha <belkinadasha@gmail.com>
2020-01-31 19:55:41 +0500

    feat: git hooks using husky

--- bin/install-hooks.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-# Enable pre-commit hook
-cp .hooks/pre-commit.sh .git/hooks/pre-commit
-chmod +x .git/hooks/pre-commit
-
-# Enable post-commit hook
-cp .hooks/post-commit.sh .git/hooks/post-commit
-chmod +x .git/hooks/post-commit
--- bin/setup.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-sh ./bin/install-hooks.sh
-cp phpdoc.dist.xml phpdoc.xml
